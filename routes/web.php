a6b7b1b6691d1fbaff7e7d6dd0658a0209651ef5
md-arif-islam <arifislamarif344@gmail.com>
2024-03-24 16:12:54 +0600

    update 2 files, create 4 files and delete 1 file

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,26 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+
+
+
+//Config cache clear
+Route::get('clear', function () {
+    Artisan::call('cache:clear');
+    Artisan::call('config:clear');
+    Artisan::call('route:clear');
+    Artisan::call('view:clear');
+    Artisan::call('optimize');
+    dd("All clear!");
 });
