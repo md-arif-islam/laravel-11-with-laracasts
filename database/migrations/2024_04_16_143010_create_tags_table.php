f297c92de459045c64d8f141c8f3b1f852bbb7c9
md-arif-islam <arifislamarif344@gmail.com>
2024-04-16 21:36:15 +0600

    Add pivot table 'job_tag' to create relationship between Job and Tag models.

--- database/migrations/2024_04_16_143010_create_tags_table.php
+++ database/migrations/2024_04_16_143010_create_tags_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Job;
+use App\Models\Tag;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +15,14 @@ return new class extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('job_tag', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Job::class, 'job_listing_id')->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Tag::class)->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
@@ -23,5 +33,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('tags');
+        Schema::dropIfExists('job_tag');
     }
 };
