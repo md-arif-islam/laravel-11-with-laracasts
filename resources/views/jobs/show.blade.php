fffef6103f25395dc0fa0887f4eff98d78bc7ef1
md-arif-islam <arifislamarif344@gmail.com>
2024-04-17 13:00:54 +0600

    Add edit and delete routes for jobs in web.php.

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -3,9 +3,15 @@
         Job
     </x-slot:heading>
 
-    <h2 class="font-bold text-lg">{{ $job['title'] }}</h2>
+    <h2 class="font-bold text-lg">{{ $job->title }}</h2>
 
     <p>
-        This job pays {{ $job['salary'] }} per year.
+        This job pays {{ $job->salary }} per year.
     </p>
+
+    <div class="mt-5">
+        <x-button href="/jobs/{{ $job->id }}/edit">
+            Edit
+        </x-button>
+    </div>
 </x-layout>
