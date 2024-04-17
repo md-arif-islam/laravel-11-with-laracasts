04cbf5413309335201b8ae409f71fbeee6aae053
md-arif-islam <arifislamarif344@gmail.com>
2024-04-17 13:25:05 +0600

    Refactor routes to use resourceful routing for JobController.

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,7 @@ use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
-Route::get('/jobs', [JobController::class, 'index']);
-Route::get('/jobs/create', [JobController::class, 'create']);
-Route::post('/jobs', [JobController::class, 'store']);
-Route::get('/jobs/{job}', [JobController::class, 'show']);
-Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
-Route::patch('/jobs/{job}', [JobController::class, 'update']);
-Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
 Route::view('/contact', 'contact');
+Route::resource('jobs', JobController::class);
 
 
