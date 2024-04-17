f715973663b0496181d73699a49a5ad723c22b45
md-arif-islam <arifislamarif344@gmail.com>
2024-04-17 12:06:52 +0600

    Update input field placeholders to include 'required' attribute for validation.

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -19,11 +19,11 @@
                                 class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                                 <input type="text" name="title" id="title"
                                        class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                       placeholder="Shift Leader">
+                                       placeholder="Shift Leader" required>
                             </div>
-                            @if($errors->has('title'))
-                                <p class="text-red-500 text-sm mt-1">{{ $errors->first('title') }}</p>
-                            @endif
+                            @error('title')
+                            <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
+                            @enderror
                         </div>
                     </div>
 
@@ -34,11 +34,12 @@
                                 class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                                 <input type="text" name="salary" id="salary"
                                        class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                       placeholder="$50,000 Per Year">
+                                       placeholder="$50,000 Per Year" required>
                             </div>
-                            @if($errors->has('salary'))
-                                <p class="text-red-500 text-sm mt-1">{{ $errors->first('salary') }}</p>
-                            @endif
+
+                            @error('salary')
+                            <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
+                            @enderror
                         </div>
                     </div>
 
