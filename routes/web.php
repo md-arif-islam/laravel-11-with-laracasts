7f713094bb5a758c24654e9bd38466473dffa8ea
md-arif-islam <arifislamarif344@gmail.com>
2024-04-17 14:47:30 +0600

    Add RegisterUserController to the web routes.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisterUserController;
 use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
@@ -8,4 +9,4 @@ Route::view('/', 'home');
 Route::view('/contact', 'contact');
 Route::resource('jobs', JobController::class);
 
-
+Route::get('/register',[RegisterUserController::class, 'create']);
